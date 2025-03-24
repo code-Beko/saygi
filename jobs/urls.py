e50568fcfb981f982c04a83d884467fee50a3899
code-Beko <berkaykasikci.code@gmail.com>
2025-03-24 14:41:41 +0300

    document add

--- jobs/urls.py
+++ jobs/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="index"),
+    path("care/", views.care, name="care"),
+    path("care/care-add/", views.care_add, name="care-add"),
 ]
--- jobs/views.py
+++ jobs/views.py
@@ -1,16 +1,13 @@
 from django.shortcuts import render
 
-from django.contrib.auth.decorators import user_passes_test
-
-
-def is_manager(user):
-    return user.groups.filter(name="Müdür").exists()
-
-
-@user_passes_test(is_manager)
-def manager_view(request):
-    return render(request, "manager_dashboard.html")
-
 
 def home(request):
     return render(request, "index.html")
+
+
+def care(request):
+    return render(request, "care.html")
+
+
+def care_add(request):
+    return render(request, "care/care_add.html")
