d46542208b8cf2ca2a74a5de5142c75c08246c9f
code-Beko <berkaykasikci.code@gmail.com>
2025-03-24 16:38:14 +0300

    forms jobs

--- jobs/forms.py
+++ jobs/forms.py
@@ -5,11 +5,36 @@ from .models import Dokuman
 class DokumanForm(forms.ModelForm):
     class Meta:
         model = Dokuman
-        fields = ["baslik", "dosya"]
+        fields = "__all__"
 
     def __init__(self, *args, **kwargs):
         super(DokumanForm, self).__init__(*args, **kwargs)
+
         for field in self.fields.values():
-            field.widget.attrs["class"] = (
-                field.widget.attrs.get("class", "") + " form-control"
+
+            if "class" not in field.widget.attrs:
+                field.widget.attrs["class"] = ""
+
+            if not isinstance(field.widget, forms.CheckboxInput):
+                field.widget.attrs["class"] += " form-control"
+
+            if isinstance(field.widget, forms.TextInput):
+                field.widget.attrs["placeholder"] = f" {field.label}"
+
+            if isinstance(field.widget, forms.DateInput):
+                field.widget.attrs["placeholder"] = "YYYY-MM-DD"
+
+            if isinstance(field.widget, forms.CheckboxInput):
+                field.widget.attrs["class"] += " form-check-input"
+
+        date_fields = [
+            "tarih",
+            "demonte_tarih",
+            "saran_onaran_tarih",
+            "monte_eden_tarih",
+            "test_eden_tarih",
+        ]
+        for field_name in date_fields:
+            self.fields[field_name].widget = forms.DateInput(
+                attrs={"type": "date", "class": "form-control"}
             )
--- jobs/models.py
+++ jobs/models.py
@@ -2,14 +2,64 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+# CustomUser modeli
 class CustomUser(AbstractUser):
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     department = models.CharField(max_length=100, blank=True, null=True)
 
 
+# Dokuman modeli
 class Dokuman(models.Model):
-    baslik = models.CharField(max_length=255)
-    dosya = models.FileField(upload_to="uploads/")
+    tersane = models.CharField(max_length=255)
+    gemi = models.CharField(max_length=255)
+    motor_ismi = models.CharField(max_length=255)
+    is_no = models.CharField(max_length=255)
+    musteri_ismi = models.CharField(max_length=255)
+    tarih = models.DateField(default="2025-01-01")
+    cihaz_teslim_alan = models.CharField(max_length=255)
+    cihaz_turu = models.CharField(max_length=255)
+    model = models.CharField(max_length=255)
+    cihaz_markasi = models.CharField(max_length=255)
+    seri_no = models.CharField(max_length=255)
+    gucu = models.CharField(max_length=255)
+    akim = models.CharField(max_length=255)
+    voltaj = models.CharField(max_length=255)
+    devir = models.CharField(max_length=255)
+    ikaz_akimi = models.CharField(max_length=255)
+    ikaz_voltaji = models.CharField(max_length=255)
+    gelis_nedeni = models.TextField()
+    parca = models.TextField()
+    eksik_malzemeler = models.TextField()
+    monte = models.BooleanField(default=False)
+    demonte = models.BooleanField(default=False)
+    kasnak_kaplin = models.BooleanField(default=False)
+    fan_tasi = models.BooleanField(default=False)
+    ayaklar = models.BooleanField(default=False)
+    kama = models.BooleanField(default=False)
+    klemens = models.BooleanField(default=False)
+    fircalar = models.BooleanField(default=False)
+    on_kapak = models.BooleanField(default=False)
+    klemens_kapagi_kut = models.BooleanField(default=False)
+    komurlar = models.BooleanField(default=False)
+    arka_kapak = models.BooleanField(default=False)
+    fan_pervane = models.BooleanField(default=False)
+    rulmanlar = models.BooleanField(default=False)
+    rulman_numarasi_on = models.CharField(max_length=255)
+    rulman_numarasi_arka = models.CharField(max_length=255)
+    demonte_eden = models.CharField(max_length=255)
+    demonte_tarih = models.DateField(default="2025-01-01")
+    saran_onaran = models.CharField(max_length=255)
+    saran_onaran_tarih = models.DateField(default="2025-01-01")
+    monte_eden = models.CharField(max_length=255)
+    monte_eden_tarih = models.DateField(default="2025-01-01")
+    test_eden = models.CharField(max_length=255)
+    test_eden_tarih = models.DateField(default="2025-01-01")
+    izolasyon_degerleri = models.TextField()
+    calisma_akimi = models.CharField(max_length=255)
+    calisma_gerilimi = models.CharField(max_length=255)
+    diger_olcum_degerleri = models.TextField()
+    test_cihazlari = models.TextField()
+
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
